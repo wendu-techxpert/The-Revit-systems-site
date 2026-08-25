af78faa919585b300e872ce69dc79151ebbeebd4
Wendu <wendutechxpert@gmail.com>
2026-08-25 11:28:18 +0100

    chnage the http tp https for other file

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,3 @@
 window.host = "the-revit-systems-site-2p44.onrender.com";
 window.localhost = "localhost:5000";
-window.baseURL = `http://${window.localhost}`;
+window.baseURL = `https://${window.host}`;
